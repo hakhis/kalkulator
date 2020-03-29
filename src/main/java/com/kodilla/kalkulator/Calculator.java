d4beb938100a9d1fa0ee802ab5316ee98449f372
hakhis <b2mieszkowski@gmail.com>
2020-03-29 21:36:55 +0200

    Zadanie: Wysłanie kodu do repozytorium

--- src/main/java/com/kodilla/kalkulator/Calculator.java
+++ src/main/java/com/kodilla/kalkulator/Calculator.java
@@ -1,4 +1,21 @@
 package com.kodilla.kalkulator;
 
 public class Calculator {
+    //private double a;
+    //private double b;
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println("It's alive!");
+        System.out.println(calculator.add(3, 2));
+        System.out.println(calculator.subtract(9, 5));
+    }
 }
